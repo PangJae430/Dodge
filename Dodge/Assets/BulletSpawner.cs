389b1ae03a4f5a65b3bd70e20dcddd7286d40f24
PangJae430 <leekj0430@naver.com>
2023-04-24 12:47:09 +0900

    2023-04-24/12:47

--- Dodge/Assets/BulletSpawner.cs
+++ Dodge/Assets/BulletSpawner.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
@@ -14,12 +12,37 @@ public class BulletSpawner : MonoBehaviour
     
     void Start()
     {
-        
+        timeAfterSpawn = 0f;
+        // 최근 생성 이후의 누적 시간을 0으로 초기화
+
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        // 탄알 생성 간격을 spawnRateMin과 spawnRateMax 사이에서 랜덤 지정
+
+        target = FindObjectOfType<PlayerController>().transform;
+        // PlayerController 컴포넌트를 가진 게임 오브젝트를 찾아 조준 대상으로 설정
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timeAfterSpawn += Time.deltaTime;
+        //timeAfterSpawn 갱신
+
+        // 최근 생성 시점에서부터 누적된 시간이 생성 주기보다 크거나 같다면
+        if (timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+            //누적된 시간을 리셋
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            // bulletPrefab의 복제본을
+            // transform.position 위치와
+            // transform.rotation 회전으로 생성
+
+            bullet.transform.LookAt(target);
+            // 생성된 bullet 게임 오브젝트의 정면 방향이 target을 향하도록 회전
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+            //다음번 생성 간격을 spawnRateMin, spawnRateMax 사이에서 랜덤 지정
+        }
     }
 }
