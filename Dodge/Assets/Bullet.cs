d45b11f72c77e71abddcecee1b69f7f8ec8dd701
PangJae430 <leekj0430@naver.com>
2023-04-24 09:17:16 +0900

    2023-04-24/09:17

--- Dodge/Assets/Bullet.cs
+++ Dodge/Assets/Bullet.cs
@@ -6,9 +6,27 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 8f;
     private Rigidbody bulletRigidbody;
+
     void Start()
     {
-        
+        bulletRigidbody = GetComponent<Rigidbody>();
+
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if(playerController!=null)
+            {
+                playerController.Die();
+            }
+        }
     }
 
     // Update is called once per frame
